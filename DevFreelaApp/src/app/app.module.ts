7be88044fbfcee3e0c6872b813530fd0876ae9b7
Marcos Aurelio Correa <marcos.correa@inmetrics.com.br>
2021-10-06 21:47:05 -0300

    Criacao de componentes angular

--- DevFreelaApp/src/app/app.module.ts
+++ DevFreelaApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
