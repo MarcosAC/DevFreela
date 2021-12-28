e0a3cc91f8b1d33a330134c9fc4867aa9221410c
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-12-27 21:50:50 -0300

    UserService

--- DevFreelaApp/src/app/services/UserService.service.ts
+++ DevFreelaApp/src/app/services/UserService.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+import { User } from '../models/User';
+import { Login } from '../models/Login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-constructor() { }
+  baseURL = `${environment.mainUrlAPI}users`;
+
+  constructor(private http: HttpClient) { }
+
+  getById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseURL}/${id}`);
+  }
+
+  post(user: User) {
+    return this.http.post(this.baseURL, user);
+  }
+
+  put(login: Login, id: number) {
+    return this.http.put(`${this.baseURL}/${id}/login`, Login);
+  }
 
 }
