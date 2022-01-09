73ca4e1a5f4b1999d4befd14af5ff7972f2f1aa4
Marcos Aurelio <marcos.aucorrea@gmail.com>
2022-01-09 11:31:37 -0300

    aaa

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -28,59 +28,99 @@ namespace DevFreela.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //var connectionString = Configuration.GetConnectionString("DevFreelaCs");
+
+            //services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
+
+            ////services.AddScoped<IProjectService, ProjectService>();
+            ////services.AddScoped<IUserService, UserService>();
+            ////services.AddScoped<ISkillService, SkillService>();
+
+            //services.AddScoped<IProjectRepository, ProjectRepository>();
+            //services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddScoped<ISkillRepository, SkillRepository>();
+
+            //services.AddControllers(options => options.Filters.Add(typeof(ValidationFilter)))
+            //    .AddFluentValidation(fluentValidation => fluentValidation.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
+
+            //services.AddMediatR(typeof(CreateProjectCommand));
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
+
+            //    //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    //{
+            //    //    Name = "Authorization",
+            //    //    Type = SecuritySchemeType.ApiKey,
+            //    //    Scheme = "Bearer",
+            //    //    BearerFormat = "JWT",
+            //    //    In = ParameterLocation.Header,
+            //    //    Description = "JWT Authorization header usando o esquema Bearer."
+            //    //});
+
+            //    //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            //    //{
+            //    //    {
+            //    //        new OpenApiSecurityScheme
+            //    //        {
+            //    //            Reference = new OpenApiReference
+            //    //            {
+            //    //                Type = ReferenceType.SecurityScheme,
+            //    //                Id = "Bearer"
+            //    //            }
+            //    //        },
+            //    //        new string[] {}
+            //    //    }
+            //    //});
+            //});
+
+            //services.AddCors();
+
+
             var connectionString = Configuration.GetConnectionString("DevFreelaCs");
-
             services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
-
-            //services.AddScoped<IProjectService, ProjectService>();
-            //services.AddScoped<IUserService, UserService>();
-            //services.AddScoped<ISkillService, SkillService>();
+            //services.AddDbContext<DevFreelaDbContext>(options => options.UseInMemoryDatabase("Devfreela"));
 
             services.AddScoped<IProjectRepository, ProjectRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ISkillRepository, SkillRepository>();
 
             services.AddControllers(options => options.Filters.Add(typeof(ValidationFilter)))
-                .AddFluentValidation(fluentValidation => fluentValidation.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
-            
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
+
             services.AddMediatR(typeof(CreateProjectCommand));
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "JWT Authorization header usando o esquema Bearer."
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
-                });
             });
-
-            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevFreela.API v1"));
+            //}
+
+            //app.UseHttpsRedirection();
+
+            //app.UseRouting();
+            //app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            ////app.UseAuthentication();
+            ////app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -91,9 +131,7 @@ namespace DevFreela.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
